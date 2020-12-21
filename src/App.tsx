4ff7a6326fe2d235e200c303e1f671f88e4a5eca
Brandon Casamichana <brandoncasawork8@gmail.com>
2020-12-21 09:15:55 -0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,76 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Button } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
 
-const Hello = () => {
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+const Home = () => {
   return (
     <div className="App">
-      <Button type="primary">Button</Button>
+      <Layout className="layout">
+        <Header className="header">
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">Tab 1</Menu.Item>
+            <Menu.Item key="2">Tab 2</Menu.Item>
+            <Menu.Item key="3">Tab 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['Directories']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="Directories">
+                <Menu.Item key="1">~</Menu.Item>
+                <Menu.Item key="2">~/usr</Menu.Item>
+                <Menu.Item key="3">option3</Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<LaptopOutlined />} title="Commands">
+                <Menu.Item key="5">option5</Menu.Item>
+                <Menu.Item key="6">option6</Menu.Item>
+                <Menu.Item key="7">option7</Menu.Item>
+                <Menu.Item key="8">option8</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="sub3"
+                icon={<NotificationOutlined />}
+                title="Macros"
+              >
+                <Menu.Item key="9">option9</Menu.Item>
+                <Menu.Item key="10">option10</Menu.Item>
+                <Menu.Item key="11">option11</Menu.Item>
+                <Menu.Item key="12">option12</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+              <Breadcrumb.Item>Terminal</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              brandoncasamichana@Brandons-MBP ~ %:
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     </div>
   );
 };
@@ -14,7 +79,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'Navbar.scss';
+
+const { Header, Content, Footer } = Layout;
+
+export default function Navbar() {
+  return <div className="Navbar"></div>;
+}
